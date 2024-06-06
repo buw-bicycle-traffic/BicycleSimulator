749a53538f6115307afba976c6a182aa252b7ef8
jackmad101 <121124425+jackmad101@users.noreply.github.com>
2024-06-06 08:49:58 +0200

    fourth commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://bisimeyerweb.onrender.com/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: 'https://bisimeyerweb.onrender.com/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
